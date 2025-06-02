a2f21c2b3655bbd3d5a453e0f9f10ca985d1ac3b
Mrs. Kikta <melissakikta@gmail.com>
2025-06-02 13:56:10 -0400

    database connection

--- server/src/dbconfig/connection.ts
+++ server/src/dbconfig/connection.ts
@@ -5,7 +5,12 @@ dotenv.config();
 
 const MONGODB_URI = process.env.MONGODB_URI || '';
 
-/* mongoose.connect(process.env.MONGODB_URI || ' */ /* mongodb://127.0.0.1:27017/ByteShift', {dbName: 'ByteShift'}); */
+/* mongoose.connect(process.env.MONGODB_URI || ' */ 
+/* mongodb://127.0.0.1:27017/ByteShift', {dbName: 'ByteShift'}); */
+
+if (!MONGODB_URI) {
+	throw new Error('MONGODB_URI is not defined in the environment variables.');
+}
 
 const db = async (): Promise<typeof mongoose.connection> => {
 	try {
