31a5f583878628ee0424f7afaa3eb1aeb4f1c1c5
Mrs. Kikta <melissakikta@gmail.com>
2025-06-05 17:59:13 -0400

    seed.ts file

--- server/src/seeds/seed.ts
+++ server/src/seeds/seed.ts
@@ -1,38 +1,49 @@
 import mongoose from 'mongoose';
 import { User, Event } from '../models/index.js'; // Adjust the import path as necessary
-import seedData from './seedData.json' with { type: "json" };
+import seedData from './seedData.json';
 import db from '../dbconfig/connection.js';
-// import { IUser } from '../models/User.js';
 import { IEvent } from '../models/Event.js';
+//import { IUser } from '../models/User.js';
+
 
 const seedDatabase = async () => {
-	await db();
+	try {
+		
+		await db();
+		
+		// Clear the database
+		await User.deleteMany();
+		await Event.deleteMany();
+		
+		// run users one at a time to hash passwords using pre hook
+		for (const user of seedData.users) {
+			const newUser = new User(user);
+			await newUser.save();
+		}
+		
+		// Insert events
+		const events:  IEvent[] = await Event.insertMany(seedData.events);
+		
+		// Assign events to users
+		const users = await User.find();
+		
+		for (const user of users) {
+			const randomEvents = events.filter(event => event.username === user.username);
+			user.events.push(...randomEvents.map(post => post._id as mongoose.Schema.Types.ObjectId));
+			
+			await user.save();
+		}
+		
+		console.log('Database seeded!');
+		mongoose.connection.close();
 
-	// Clear the database
-	await User.deleteMany();
-	await Event.deleteMany();
-
-	// run users one at a time to hash passwords using pre hook
-	for (const user of seedData.users) {
-		const newUser = new User(user);
-		await newUser.save();
-	}
-	const events: IEvent[] = await Event.insertMany(seedData.events);
-
-	// Assign events to users
-	const users = await User.find();
-	//   randomly assign events to users
-	for (const user of users) {
-		const randomEvents = events.filter(event => event.username === user.username);
-		user.events.push(...randomEvents.map(event => event._id as mongoose.Schema.Types.ObjectId));
-		await user.save();
-	}
-
-	console.log('Database seeded!');
-	mongoose.connection.close();
+	} catch (err) {
+		console.error('Error seeding database:', err);
+		mongoose.connection.close();
+	}	
 };
 
 seedDatabase().catch(err => {
-	console.error(err);
+	console.error('Seed Error:', err);
 	mongoose.connection.close();
 });
